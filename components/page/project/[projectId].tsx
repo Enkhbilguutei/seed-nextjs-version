f4318496643ebf342e4c44aea00c018ece9af979
Enkhburen Enkhbayar <hreverse2@gmail.com>
2022-05-02 09:14:12 +0800

    unnecessary files removed

--- components/page/project/[projectId].tsx
+++
@@ -1,3 +0,0 @@
-export default function projectDetails() {
-	return <h1>Details Project</h1>
-}
--- components/page/project/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function projectDetails() {
-	return (
-		<>
-			<h2>Project 1</h2>
-			<h2>Project 2</h2>
-			<h2>Project 3</h2>
-		</>
-	)
-}
